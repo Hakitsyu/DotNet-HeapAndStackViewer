0d2a739984b3dd36bc7e40e37566ce13b37bec0e
Vitor Jesus <vitorjdev@gmail.com>
2024-07-24 20:24:30 -0300

    initial commit

---
+++ ConsoleReader.cs
@@ -0,0 +1,54 @@
+﻿namespace DotNet_HeapAndStackViewer
+{
+    internal static class ConsoleReader
+    {
+        internal static string? GetText(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+
+        internal static string GetRequiredText(string text, string? retryMessage = null)
+        {
+            Console.Write(text);
+            return ForceReadLine(retryMessage);
+        }
+
+        internal static string GetOption(string text, 
+            IEnumerable<string> options,
+            string? retryMessage = null)
+        {
+            if (!text.EndsWith(' '))
+                text = text + ' ';
+
+            Console.Write(text);
+
+            while (true)
+            {
+                var option = ForceReadLine(retryMessage).Trim();
+                if (options.Contains(option))
+                    return option;
+
+                if (!string.IsNullOrWhiteSpace(retryMessage))
+                    Console.WriteLine(retryMessage);
+            }
+        }
+
+        internal static string ForceReadLine(string? retryMessage = null)
+        {
+            string? content = null;
+
+            do
+            {
+                content = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(retryMessage) && string.IsNullOrWhiteSpace(content))
+                {
+                    Console.WriteLine(retryMessage);
+                }
+
+            } while (string.IsNullOrWhiteSpace(content));
+
+            return content;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using DotNet_HeapAndStackViewer;
+using Microsoft.Diagnostics.Runtime;
+using System.Diagnostics;
+
+while (true)
+{
+    var processId = GetProcessIdOrDefault();
+    if (processId == null)
+        return;
+
+    var dataTarget = DataTarget.CreateSnapshotAndAttach(processId.Value);
+
+    foreach (var clr in dataTarget.ClrVersions)
+    {
+        Console.WriteLine($"CLR {clr.Version}");
+
+        var runtime = clr.CreateRuntime();
+        foreach (var thread in runtime.Threads)
+        {
+            Console.WriteLine($"    Thread {thread.ManagedThreadId}");
+
+            foreach (var stack in thread.EnumerateStackTrace())
+            {
+                Console.WriteLine($"    Stack {stack.StackPointer}{Environment.NewLine}" +
+                    $"      Instruction Pointer: {stack.InstructionPointer}{Environment.NewLine}" +
+                    $"      Kind: {stack.Kind}{Environment.NewLine}" +
+                    $"      Method: {stack.Method}{Environment.NewLine}" +
+                    $"      Frame: {stack.FrameName}{Environment.NewLine}" +
+                    $"      Context: {stack.Context.ToString()}");
+            }
+        }
+
+        Console.WriteLine($"    Heap:");
+        foreach (var heap in runtime.Heap.EnumerateObjects())
+        {
+            Console.WriteLine($"    Object:{Environment.NewLine}" +
+                $"      Type: {heap.Type}{Environment.NewLine}" +
+                $"      Size: {heap.Size}{Environment.NewLine}" +
+                $"      Address: {heap.Address}{Environment.NewLine}" +
+                $"      IsNull: {heap.IsNull}{Environment.NewLine}");
+        }
+    }
+}
+
+static int? GetProcessIdOrDefault()
+{
+    var processId = ConsoleReader.GetText("Process Id: ");
+
+    if (string.IsNullOrWhiteSpace(processId))
+    {
+        const string yesOption = "y";
+        const string noOption = "n";
+
+        var option = ConsoleReader.GetOption(
+            "If a process ID is not provided, the current process will be used for experiment. Do you want to continue (y/n)?",
+            [yesOption, noOption]);
+
+        return option == yesOption
+            ? Process.GetCurrentProcess().Id
+            : null;
+    }
+
+    if (int.TryParse(processId, out int result)) 
+        return result;
+
+    return GetProcessIdOrDefault();
+}
